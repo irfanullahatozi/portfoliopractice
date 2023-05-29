beafdc1abd2b8b3c447dcb16678f1cbf4e27f62e
irfanullahtroon <irfan@troontechnologies.com>
2023-05-29 21:54:00 +0500

    adding navbar structure

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
+   <div className="n-wrapper">
+    <div className="n-left">
+        <div className="n-name">Irfan</div>
+        <span>toggle</span>
     </div>
+    <div className="n-right">
+        <div className="n-list">
+            <ul>
+                <li>Home</li>
+                <li>Services</li>
+                <li>Experience</li>
+                <li>Portfolio</li>
+                <li>Testimonial</li>
+            </ul>
+        </div>
+        <button className="button">
+            Contact us
+        </button>
+    </div>
+   </div>
   )
 }
 
